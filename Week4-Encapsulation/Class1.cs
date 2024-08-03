3544372f96acffd6dd392a5b0c8571ee63e2f111
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-03 15:07:38 +0300

    Add project files.

---
+++ Week4-Encapsulation/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Encapsulation
+{
+
+
+	public class Car
+	{
+
+		public Car(string marka, string model, string renk, int doornumber)
+        {
+			Marka = marka;
+			Model = model;
+			Renk = renk;
+			DoorNumber = doornumber;
+        }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Renk { get; set; }
+
+        private int _doorNumber;
+
+		public int DoorNumber
+		{
+			get { return _doorNumber; }
+			set
+			{ 
+				if ((value != 2 && value != 4))
+				{
+                    Console.WriteLine("Yanlış kapı değeri girdin....");
+					_doorNumber = -1;
+                }
+				else
+				{
+                    _doorNumber = value;
+
+                }
+                
+			}
+		}
+
+		public void CarInfo()
+		{
+            Console.WriteLine($"_____________________________\n Arabanın Modeli:{Marka}\n Markası {Model}\n Rengi : {Renk}\n Kapı Sayısı : {DoorNumber}");
+        }
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Week4-Encapsulation/Program.cs
@@ -0,0 +1,5 @@
+﻿using System;
+using Week4_Encapsulation;
+
+Car car1 = new("Tofaşk", "Doğan-Slx", "Beyaz", 3);
+car1.CarInfo();
